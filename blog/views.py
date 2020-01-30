a450c80686a4212348b5cc428fa3c37f9fd8d84b
Alexey <gradiviro@gmail.com>
2020-01-30 12:29:50 +0200

    signup functionality

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 def post_list(request):
@@ -75,3 +78,8 @@ def LoginView(request):
 
 def LogoutView(request):
     logout(request)        
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'    
